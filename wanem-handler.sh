44f609ffc1fe740b70bbe319c512c704f4e24038
hbrust <helge.brust@citrix.com>
2020-11-28 07:33:01 +0100

    improvements

--- wanem-handler.sh
+++ wanem-handler.sh
@@ -8,6 +8,18 @@ do
         dev="${arg#*=}"
         shift
         ;;
+        -dt=*|--delay_time=*)
+        delay_time="${arg#*=}"
+        shift
+        ;;
+        -jr=*|--jitter_rate=*)
+        jitter="${arg#*=}"
+        shift
+        ;;
+        -dc=*|--delay_correlation=*)
+        delay_corr="${arg#*=}"
+        shift
+        ;;
         -lr=*|--loss_rate=*)
         loss_rate="${arg#*=}"
         shift
@@ -18,10 +30,6 @@ do
         ;;
         -cr=*|--corruption_rate=*)
         corrupt_rate="${arg#*=}"
-        shift
-        ;;
-        -cc=*|--corruption_correlation=*)
-        corrupt_corr="${arg#*=}"
         shift
         ;;
         -d=*|--duration=*)
@@ -37,22 +45,6 @@ if [ -z "$dev" ]; then
   exit 1
 fi
 
-if [ -z "$loss_rate" ]; then
-  loss_rate="0%"
-fi
-
-if [ -z "$loss_corr" ]; then
-  loss_corr="0%"
-fi
-
-if [ -z "$corrupt_rate" ]; then
-  corrupt_rate="0%"
-fi
-
-if [ -z "$corrupt_corr" ]; then
-  corrupt_corr="0%"
-fi
-
 # get old config
 rule_show=$( tc qdisc show dev ${dev} | cut -d  ":" -f2 )
 rule_class=$( echo $rule_show | cut -d " " -f1 )
@@ -60,23 +52,96 @@ rule_old=$( echo $rule_show | cut -d " " -f2-100 )
 if [[ $rule_old == bands* ]]; then
 	rule_old=""
 fi
+rule_old_params=$rule_old
+
+
+# set default
+$limit_param="1000"
+$delay_param="0 0 0"
+$loss_param="0 0"
+$corrupt_param="0"
+
+
+# get old parameter
+for param in $rule_old_params
+do
+  case $param in
+    limit)
+      shift
+      limit_param=()
+      next="limit_param"
+      ;;
+    delay)
+      shift
+      delay_param=()
+      next="delay_param"
+      ;;
+    loss)
+      shift
+      loss_param=()
+      next="loss_param"
+      ;;
+    corrupt)
+      shift
+      corrupt_param=()
+      next="loss_param"
+      ;;
+    [0-9]*)
+      eval "$next+=\"\$param \""
+      ;;
+    *)
+      ;;
+  esac
+done
+echo "Limit: "$limit_param
+echo "Delay: "$delay_param
+echo "Loss: "$loss_param
+echo "Corr: "$corrupt_param
+
 
 # build new config
-cmd="tc qdisc replace dev ${dev}"
-new_rule="${rule_old} loss ${loss_rate} ${loss_corr} corrupt ${corrupt_rate} ${corrupt_corr}"
+base_cmd="tc qdisc replace dev ${dev}"
+
+if [ ! -z $limit ]; then
+  delay_cmd="limit ${limit}"
+else
+  delay_cmd="delay ${delay_param}"
+fi
+
+if [ ! -z $delay_time ]; then
+  delay_cmd="delay ${delay_time} ${jitter} ${delay_corr}"
+else
+  delay_cmd="delay ${delay_param}"
+fi
+
+if [ ! -z $loss_rate ]; then
+  loss_cmd="loss ${loss_rate} ${loss_corr}"
+else
+  loss_cmd="loss ${loss_param}"
+fi
+
+if [ ! -z $corrupt_time ]; then
+  corrupt_cmd="corrupt ${corrupt_rate}"
+else
+  corrupt_cmd="corrupt ${corrupt_param}"
+fi
+
+new_rule="${delay_cmd} ${loss_cmd} ${corrupt_cmd}"
 new_cmd="${cmd} ${rule_class} netem ${new_rule}"
 
 # print out information
-echo "Rule:  " $rule_old
+echo "Rule: " $new_rule
 echo "Class: " $rule_class
 echo "CMD:   " $new_cmd
 
 # execute CMD
 $new_cmd
 
-if [ ! -z "$duration" ];
+if [ ! -z "$duration" ]; then
   sleep $duration
-  $new_cmd="${cmd} ${rule_class} ${rule_old}"
+  $new_cmd="${cmd} ${rule_class} netem ${rule_old}"
+  echo $new_cmd
+  $new_cmd
 fi
 
 exit 0
